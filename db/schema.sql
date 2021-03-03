099fac06afcd9c04017602c64bebe2f11933ae85
Sara Taylor <sara.swifti@gmail.com>
2021-03-02 19:09:30 -0500

    finishing up with schema and seed file

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+//creating database, tables and indexes into each
+
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
@@ -28,3 +30,37 @@ CREATE TABLE employee (
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
+////schema for posting to instert into queries
+SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
+SELECT role.id, role.title FROM role ORDER BY role.id;
+SELECT * FROM employee;
+
+SELECT department.id, department.name FROM department ORDER BY department.id;
+
+SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY department.name;
+    
+SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+  FROM employee
+  LEFT JOIN employee manager on manager.id = employee.manager_id
+  INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+  INNER JOIN department ON (department.id = role.department_id)
+  ORDER BY manager;
+  
+SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN role ON (role.id = employee.role_id)
+    LEFT JOIN department ON (department.id = role.department_id)
+    ORDER BY role.title;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+  FROM employee
+  LEFT JOIN employee manager on manager.id = employee.manager_id
+  INNER JOIN role ON (role.id = employee.role_id)
+  INNER JOIN department ON (department.id = role.department_id)
+  ORDER BY employee.id;
+  
+SELECT first_name, last_name, role_id FROM employee 	WHERE employee.id = 4;
--- db/seed.sql
+++ db/seed.sql
@@ -1,33 +1,33 @@
-DROP DATABASE IF EXISTS employ_trackddb;
-CREATE database employ_trackdb;
+///got the structure from another student and made it my own inserting from top down starting with department and then into role and then into employee. 
 
-USE employ_trackdb;
-
-CREATE TABLE top5000 (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  song VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
-);
-
-CREATE TABLE top_albums (
-  position INT NOT NULL,
-  artist VARCHAR(100) NULL,
-  album VARCHAR(100) NULL,
-  year INT NULL,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
-  PRIMARY KEY (position)
-);
-
-SELECT * FROM top5000;
-select * from top_albums;
+use employees;
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Research and Development'),
+    ('Manufacturing'),
+    ('Distribution');
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Field Manager', 11000000, 1),
+    ('Sales Rep', 7000000, 1),
+    ('Project Manager', 15000000, 2),
+    ('Engineer', 8000000, 2),
+    ('Foreman', 7000000, 3),
+    ('Maintenance', 3000000, 3),
+    ('Shipping Specialist', 12000000, 4),
+    ('Warehouse Staff', 2500000, 4);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Stienbeck', 1, NULL),
+    ('Lowell', 'Swift', 2, 1),
+    ('Amy', 'Churchill', 3, NULL),
+    ('Sarah', 'DeMurcurio', 4, 3),
+    ('Mady', 'Taylor', 5, NULL),
+    ('Julia', 'Green', 6, 5),
+    ('Vickee', 'Shulman', 7, NULL),
+    ('Oprah', 'Winfrey', 8, 7);
+    
