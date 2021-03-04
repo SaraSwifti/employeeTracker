b75296402363dc352ea169cfd8c9c74f32f2f6ce
Sara Taylor <sara.swifti@gmail.com>
2021-03-03 21:54:17 -0500

    working out the bugs

--- index.js
+++ index.js
@@ -10,28 +10,38 @@ app.use(express.json());
 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: 'root',
-  
-    // Be sure to update with your own MySQL password!
-    password: 'SaraSwifti!12',
-    database: 'employees_db'
-  });
-  
-  connection.connect((err) => {
-    if (err) {
-      console.error('error connecting')
-    }
-    console.log(`connected as id ${connection.threadId}`);
-    //runSearch();
-  });
+  host: 'localhost',
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: 'root',
+
+  // Be sure to update with your own MySQL password!
+  password: 'SaraSwifti!12',
+  database: 'employees_db'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting')
+  }
+  console.log(`connected as id ${connection.threadId}`);
+  //runSearch();
+});
 
 
+connection.query("SELECT * from role", function (error, res) {
+  showroles = res.map(role => ({ name: role.title, value: role.id }))
+})
+connection.query("SELECT * from department", function (error, res) {
+  showdepartments = res.map(dep => ({ name: dep.name, value: dep.id }))
+})
+connection.query("SELECT * from employee", function (error, res) {
+  // console.log(error, res);
+  showemployees = res.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }))
+})
 
 
 //creaete questioning thread through inquirer with corresponding  then statements to direct to the correct functions once passed through a switch statement
@@ -68,86 +78,6 @@ function firstMenu() {
   });
 };
 
-const addDepartment = () => {
-
-  inquirer.prompt([
-    {
-      type: 'input',
-      name: 'department',
-      message: 'What is the name of the department you want to add?',
-    },
-  ]).then(function (response) {
-    let query = "INSERT INTO department (name) VALUES (?)";
-    connection.query(
-      query,
-      response.department,
-      (err, res) => {
-        if (err) throw err;
-        console.log(`The ${(response.department).toUpperCase()} department has been added`)
-        // {connection.query(
-        //     'INSERT INTO department'
-        // )
-        //     console.log(`Adding ${response.name} to department catagory`)
-        // }
-
-        firstMenu();
-      }
-    );
-  });
-}
-
-function addRole() {
-
-  inquirer.prompt([
-      {
-          type: 'input',
-          name: 'title',
-          message: 'What is the title of this new role?',
-      },
-      {
-          type: 'input',
-          name: 'salary',
-          message: 'What is the salary of this new role',
-      },
-      {
-          type: 'list',
-          message: 'Which department is this new role in?',
-          name: 'menu',
-          choices: ['Engineer', 'Intern', 'Finished'],
-      }
-
-  ]).then(function(response){
-
-      //now to choice menu for adding another empoloyee or exiting the funciton. 
-      firstMenu();
-  });
-};
-//function for adding an Employee
-function addEmploy() {
-  inquirer.prompt([
-      {
-          type: 'input',
-          name: 'first_name',
-          message: 'What is the first name of the employee being added?',
-      },
-      {
-          type: 'input',
-          name: 'last_name',
-          message: 'What is the last name of the employee being added?',
-      },
-      {
-          type: 'list',
-          message: 'Which department is this new role in?',
-          name: 'menu',
-          choices: ['Engineer', 'Intern', 'Finished'],
-      }
-
-  ]).then(function(response){
-
-      //now to choice menu for adding another empoloyee or exiting the funciton. 
-      firstMenu();
-  });
-};
 // view Department function
 
 function viewDept() {
@@ -157,23 +87,132 @@ function viewDept() {
     console.table(res);
     firstMenu();
   });
-  
+
 };
 // view Roles
-function viewDept(){
+function viewRoles() {
   connection.query("SELECT * from role", function (error, res) {
     console.table(res);
     endOrMenu();
   })
 };
 
-
 //view Employee
-function viewEmploy(){
-  connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (error, res) {
-    console.table(res);
+function viewEmploy() {
+  connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+    function (error, res) {
+      console.table(res);
+      firstMenu();
+    })
+}
+function addDepartment() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'department',
+      message: 'What is the name of the department you want to add?',
+    },
+  ]).then(function (response) {
+    addDept(response);
+  });
+};
+
+function addDept() {
+  let query = "INSERT INTO department (name) VALUES (?)";
+  connection.query(
+    query,
+    response.department,
+    (err, res) => {
+      if (err) throw err;
+      console.log(`The ${(response.department).toUpperCase()} department has been added`)
+
+      firstMenu();
+    }
+  );
+};
+
+
+function addRole() {
+
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of this new role?',
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'What is the salary of this new role',
+    },
+    {
+      type: 'list',
+      message: 'Which department is this new role in?',
+      name: 'menu',
+      choices: ['Engineer', 'Intern', 'Finished'],
+    }
+  ]).then(function (response) {
+
+    aaddRole(response);
+  });
+};
+
+function aaddRole() {
+  connection.query("INSERT INTO role SET ?", {
+    title: response.title,
+    salary: response.salary,
+    department_id: response.id
+  }, function (error, res) {
+    // console.log(error, res);
+    if (error) throw error;
     firstMenu();
-  })
+  });
+
+
+};
+//function for adding an Employee
+function addEmploy() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: "What is the first name?",
+      name: "firstName",
+    },
+    {
+      type: "input",
+      message: "What is the last name?",
+      name: "lastName",
+    },
+    {
+      type: "list",
+      message: "What is the employee's title?",
+      name: "title",
+      choices: showroles
+    },
+    {
+      type: "list",
+      message: "Who is the employee's manager?",
+      name: "manager",
+      choices: showemployees,
+    }
+  ]).then(function (response) {
+    aaddEmploy(response);
+  });
+};
+
+// add Employ
+function aaddEmploy() {
+  connection.query("INSERT INTO employee SET ?",
+    {
+      first_name: response.firstName,
+      last_name: response.lastName,
+      role_id: response.title,
+      manager_id: response.manager
+    }, function (error, res) {
+      if (error) throw error;
+    });
+  firstMenu();
+};
 
 
 // ///function update employee role
@@ -190,10 +229,10 @@ function viewEmploy(){
 //       choices: res.map(function(nam) {
 // return nam.
 //       })
-      
+
 //       function() {
 //         employeeArray = [];
-        
+
 //            res.map(res => {
 //                employeeArray.push(
 //                    res.last_name
